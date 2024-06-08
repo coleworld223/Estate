bf84f1ea858e51ef103e9db496cf388a77db2929
anuj <anujanand223@gmai.com>
2024-06-08 09:54:53 +0530

    created post signup api route

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -1,11 +1,13 @@
 import bcryptjs from 'bcryptjs';
 import User from '../models/user.model.js'
+import { errorHandler } from '../utils/error.js';
 
 export const signup = async (req, res, next) => {
+    console.log (req)
     const { username, email, password } = req.body;
     const hashedPassword = bcryptjs.hashSync(password, 10);
     const newUser = new User({ username, email, password: hashedPassword });
-    try {User
+    try {
       await newUser.save();
       res.status(201).json('User created successfully!');
     } catch (error) {
